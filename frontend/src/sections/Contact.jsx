fe5ef1ba23abe5cbdeb762770b44c49f211d0327
Samson V <149072583+HappySamson@users.noreply.github.com>
2026-02-28 13:53:12 +0530

    Update Contact.jsx

--- frontend/src/sections/Contact.jsx
+++ frontend/src/sections/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
     setStatus("");
 
     try {
-      await axios.post("http://localhost:5000/api/contacts", formData);
+      await axios.post("https://symposium-management.onrender.com/api/contacts/api/contacts", formData);
 
       setStatus("success");
       setFormData({
