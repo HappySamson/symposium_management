0ad8749ecbfd9255af5704fe980a06fe492ea1a5
Samson V <149072583+HappySamson@users.noreply.github.com>
2026-02-28 14:09:19 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://symposium-management.onrender.com/api"
+  baseURL: "https://symposium-management.onrender.com/"
 });
 
 export default API;
