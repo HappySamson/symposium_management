5aeab843c5a5a3e63eb189af9f700dd0adcd1643
Samson V <149072583+HappySamson@users.noreply.github.com>
2026-02-28 13:55:11 +0530

    Update ScheduleSection.jsx

--- frontend/src/sections/ScheduleSection.jsx
+++ frontend/src/sections/ScheduleSection.jsx
@@ -11,7 +11,7 @@ export default function ScheduleSection() {
 
   const fetchSchedules = async () => {
     try {
-      const res = await axios.get("http://localhost:5000/api/schedule");
+      const res = await axios.get("https://symposium-management.onrender.com/api/contacts/api/schedule");
       setSchedules(res.data);
     } catch (err) {
       console.error("Error fetching schedule:", err);
