54b66f0c5fe7a3a19504a291c6ceb29496782450
Samson V <149072583+HappySamson@users.noreply.github.com>
2026-02-28 13:30:53 +0530

    Update EditEventModal.jsx

--- frontend/src/admin/EditEventModal.jsx
+++ frontend/src/admin/EditEventModal.jsx
@@ -34,7 +34,7 @@ export default function EditEventModal({ event, onClose, refresh }) {
   const updateEvent = async () => {
     try {
       await axios.put(
-        `http://localhost:5000/api/events/${form._id}`,
+        `https://symposium-management.onrender.com/api/events/${form._id}`,
         form
       );
 
